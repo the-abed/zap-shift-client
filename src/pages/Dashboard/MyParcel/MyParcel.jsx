34efc6e77c6770baf7303c98227ea82f5dba24b9
the-abed <mohammadabed1283@gmail.com>
2025-11-24 23:01:28 +0600

    add parcel delete function

--- src/pages/Dashboard/MyParcel/MyParcel.jsx
+++ src/pages/Dashboard/MyParcel/MyParcel.jsx
@@ -2,18 +2,63 @@ import React from "react";
 import useAuth from "../../../hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaRegEdit } from "react-icons/fa";
+import { IoTrashOutline } from "react-icons/io5";
+import Swal from "sweetalert2";
 
 const MyParcel = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["myParcel", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels?email=${user?.email}`);
       return res.data;
     },
   });
+
+  const handleDeleteParcel = (id) => {
+    Swal.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+    
+    axiosSecure.delete(`/parcels/${id}`)
+    .then(res => {
+      if(res.data.deletedCount){
+        // refresh the data after delete
+        refetch();
+        Swal.fire(
+          'Deleted!',
+          'Your parcel has been deleted.',
+          'success'
+        )
+      }
+    })
+
+    // Swal.fire({
+    //   title: "Deleted!",
+    //   text: "Your file has been deleted.",
+    //   icon: "success"
+    // });
+  }
+});
+    
+  }
+
+//   Calculate total cost
+  const totalCost = parcels.reduce(
+    (sum, item) => sum + (item.cost ? Number(item.cost) : 0),
+    0
+  );
 
   return (
     <div className="p-6">
@@ -23,7 +68,7 @@ const MyParcel = () => {
 
       {/* Table */}
       <div className="overflow-x-auto border-1 border-gray-200 rounded-md">
-        <table className="table table-zebra w-full">
+        <table className="table table-zebra w-full text-center">
           <thead className="bg-base-200 text-lg font-semibold">
             <tr>
               <th>#</th>
@@ -32,6 +77,7 @@ const MyParcel = () => {
               <th>Parcel Weight</th>
               <th>Cost</th>
               <th>Status</th>
+             <th>Actions</th>
             </tr>
           </thead>
 
@@ -43,15 +89,37 @@ const MyParcel = () => {
                 <td>{parcel.parcelType}</td>
 
                 {/* If parcel weight exists, show it — else show "N/A" */}
-                <td className="text-center">{parcel.parcelWeight ? parcel.parcelWeight : "N/A"}</td>
+                <td className="text-center">
+                  {parcel.parcelWeight ? parcel.parcelWeight : "N/A"}
+                </td>
 
                 {/* If cost exists, show it — else show "N/A" */}
                 <td>{parcel.cost ? parcel.cost : "N/A"}</td>
 
                 {/* If status exists, show it — else default to "Pending" */}
                 <td>{parcel.status ? parcel.status : "Pending"}</td>
+
+                  {/* Action Buttons (View, Edit and Delete) */}
+                <td className="flex justify-center gap-2">
+                    <button className="btn btn-square tooltip hover:tooltip-bottom hover:bg-primary" data-tip="View"><FaMagnifyingGlass /></button>
+                    <button className="btn btn-square tooltip hover:tooltip-bottom hover:bg-primary" data-tip="Edit"><FaRegEdit /></button>
+                    <button
+                     onClick={() => handleDeleteParcel(parcel._id)}
+                     className="btn btn-square tooltip hover:tooltip-bottom hover:bg-primary" data-tip="Delete"><IoTrashOutline /></button>
+                </td>
+               
               </tr>
+
+               
             ))}
+
+            {/* Total Cost Row */}
+            <tr className="bg-base-100 font-bold text-lg ">
+              <td colSpan={3}></td>
+              <td colSpan={2}></td>
+              <td colSpan={1}>Total Cost: {totalCost}</td>
+            </tr>
+
           </tbody>
         </table>
       </div>
