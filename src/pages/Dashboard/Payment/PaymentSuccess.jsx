fffbe5ca57ef4a5ea94fba3ee9d32b4079a9c624
the-abed <mohammadabed1283@gmail.com>
2025-11-25 23:20:34 +0600

    add payment success and cencelled page

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,27 +1,35 @@
-import React, { useEffect } from 'react';
-import { useSearchParams } from 'react-router';
-import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
-    const axiosSecure = useAxiosSecure();
-    const [searchParams] = useSearchParams();
-    const sessionId = searchParams.get('session_id');
-    console.log(sessionId)
+  const [paymentInfo, setPaymentInfo] = useState();
+  const axiosSecure = useAxiosSecure();
+  const [searchParams] = useSearchParams();
+  const sessionId = searchParams.get("session_id");
+  console.log(sessionId);
 
-    useEffect(() => {
-        if(sessionId){
-            axiosSecure.patch(`/payment-success?session_id=${sessionId}`)
-            .then(res => {
-                console.log(res.data); 
-            })
-        }
-    }, [sessionId, axiosSecure])
+  useEffect(() => {
+    if (sessionId) {
+      axiosSecure
+        .patch(`/payment-success?session_id=${sessionId}`)
+        .then((res) => {
+          console.log(res.data);
+          setPaymentInfo({
+            transactionId: res.data.transactionId,
+            trackingId: res.data.trackingId,
+          });
+        });
+    }
+  }, [sessionId, axiosSecure]);
 
-    return (
-        <div>
-            <h2 className="text-3xl">Payment Success</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-3xl">Payment Success</h2>
+      <p>Transaction ID: {paymentInfo?.transactionId}</p>
+      <p>Tracking ID: {paymentInfo?.trackingId}</p>
+    </div>
+  );
 };
 
 export default PaymentSuccess;
